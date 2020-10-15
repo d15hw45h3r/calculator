be9dc09e30a91001412603d5ef6b61c26cca4f56
Valeriia <43512665+d15hw45h3r@users.noreply.github.com>
2020-10-15 15:07:12 +0300

    Updates

---
+++ precache-manifest.7018106d6edd70da631a896ecba47a6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d6d9bf8086fb9e552badee3ef540f18",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "8d74cedebe4e72f466b0",
+    "url": "/calculator/static/css/main.47e0b5a1.chunk.css"
+  },
+  {
+    "revision": "57b8164334731c8ba487",
+    "url": "/calculator/static/js/2.99e710fb.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/calculator/static/js/2.99e710fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d74cedebe4e72f466b0",
+    "url": "/calculator/static/js/main.fa5572d3.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  },
+  {
+    "revision": "ceeee93b9aabe37d8b3431239384ef4b",
+    "url": "/calculator/static/media/background.ceeee93b.jpg"
+  }
+]);
